6a5c00efe79a141d6432342757e6fd14abcf6113
Peter Esselius <pepp@me.com>
2025-10-14 20:16:27 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+}
